5a30b8ab06a493e6e6e567d6e6c8d229f7b8a634
RuslanMirasov <info@mirasov.dev>
2023-05-30 18:26:12 +0200

    effect schake on hover

--- src/js/burger_menu.js
+++ src/js/burger_menu.js
@@ -1,15 +1,17 @@
-const openMenuBtn = document.querySelectorAll('[data-menu-open]');
-const menu = document.querySelector('.menu-backdrop');
-const burger = document.querySelector('.burger');
-const body = document.querySelector('.body');
+document.addEventListener('DOMContentLoaded', event => {
+  const openMenuBtn = document.querySelectorAll('[data-menu-open]');
+  const menu = document.querySelector('.menu-backdrop');
+  const burger = document.querySelector('.burger');
+  const body = document.querySelector('.body');
 
-// MOBIL MENU OPEN / CLOSE
-openMenuBtn.forEach(openBtn => {
-  openBtn.addEventListener('click', menuToggle);
+  // MOBIL MENU OPEN / CLOSE
+  openMenuBtn.forEach(openBtn => {
+    openBtn.addEventListener('click', menuToggle);
+  });
+
+  function menuToggle() {
+    menu.classList.toggle('is-open');
+    burger.classList.toggle('is-open');
+    body.classList.toggle('lock');
+  }
 });
-
-function menuToggle() {
-  menu.classList.toggle('is-open');
-  burger.classList.toggle('is-open');
-  body.classList.toggle('lock');
-}
--- src/js/films_hover.js
+++ src/js/films_hover.js
@@ -1,29 +1,39 @@
-const films = document.querySelectorAll('.js-film-hover');
-const posters = document.querySelectorAll('.poster');
+document.addEventListener('DOMContentLoaded', event => {
+  const films = document.querySelectorAll('.js-film-hover');
+  const posters = document.querySelectorAll('.poster');
 
-films.forEach(filmLink => {
-  //desctop events
-  filmLink.addEventListener('mouseover', showMoviePoster);
-  filmLink.addEventListener('mouseout', clearMoviePosters);
-  //mobil events
-  filmLink.addEventListener('touchstart', showMoviePoster);
-  filmLink.addEventListener('touchend', clearMoviePosters);
+  films.forEach(filmLink => {
+    //desctop events
+    filmLink.addEventListener('mouseover', showMoviePoster);
+    filmLink.addEventListener('mouseout', clearMoviePosters);
+    //mobil events
+    filmLink.addEventListener('touchstart', showMoviePoster);
+    filmLink.addEventListener('touchend', clearMoviePostersMobil);
+  });
+
+  function clearMoviePosters() {
+    posters.forEach(poster => {
+      poster.classList.remove('active');
+    });
+  }
+
+  function clearMoviePostersMobil() {
+    const filmId = this.dataset.id;
+    posters.forEach(poster => {
+      if (poster.dataset.id !== filmId) {
+        poster.classList.remove('active');
+      }
+    });
+  }
+
+  function showMoviePoster() {
+    const filmId = this.dataset.id;
+    posters.forEach(poster => {
+      if (poster.dataset.id === filmId) {
+        poster.classList.add('active');
+      } else {
+        poster.classList.remove('active');
+      }
+    });
+  }
 });
-
-function clearMoviePosters() {
-  posters.forEach(poster => {
-    poster.classList.remove('active');
-  });
-}
-
-function showMoviePoster() {
-  const filmId = this.dataset.id;
-  posters.forEach(poster => {
-    if (poster.dataset.id === filmId) {
-      poster.classList.add('active');
-    }
-  });
-}
-
-// console.log(films);
-// console.log(posters);
